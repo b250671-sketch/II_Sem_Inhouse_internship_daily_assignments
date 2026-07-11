743f035638f386bc53d7be00ee8fb8d78aacd16e
b250671-sketch <b250671@gmail.com>
2026-07-11 21:05:25 +0530

    Add day6

---
+++ 25ESKCS379/Day6/script.js
@@ -0,0 +1,24 @@
+ let students =[
+       {
+           
+             name:'yashwini',
+            active:true,
+            branch: 'CS',
+           rollno  :379,
+            marks:99
+          },
+
+
+        
+            {
+            name: 'vanshika',
+            branch :'CS',
+            rollno :361,
+           marks :98
+            }
+       ]
+       console.log( JSON.stringify(students))
+      let student=  [{"name":"yashwini","active":true,"branch":"CS","rollno":379,"marks":99},{"name":"vanshika","branch":"CS","rollno":361,"marks":98}]
+      console.log(JSON.parse)
+
+        
